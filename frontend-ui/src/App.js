35b39737523587ee3d2144bc453b99bc18f1c6f1
PanayiotisCS <panayiotis.cs@gmail.com>
2022-07-07 19:38:17 +0300

    Added routes for home page

--- frontend-ui/src/App.js
+++ frontend-ui/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import './temp.css';
+import {Home} from './Screens/Home';
+import { Login } from './Screens/Login';
+import {BrowserRouter, Route, Routes, NavLink} from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <div className='container-fluid background'>
+          <nav className='navbar navbar-expand-sm navbar-dark'>
+            <ul className='navbar-nav'>
+              <li className='nav-item-'>
+                  <NavLink to='/home'>
+                    <img className='line img-thumnail' src='/logo-white-stacked-en.svg' width={120}/>
+                  </NavLink>
+                    <h4 className='nav-header mx-3 pt-4'>Scholarship Submission</h4>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <Routes>
+          <Route path='/' component={Home}/>
+          <Route path='/home' component={Home}/>
+          <Route path='/login' component={Login}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend-ui/src/Screens/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom/client';
+
+export class Home extends React.Component{
+    render(){
+        return(
+            <div>
+                <h2>Home Page</h2>
+            </div>
+        )
+    }
+};
